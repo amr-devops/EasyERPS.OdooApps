1f4185364c9b95ae6aeeeeea1223638fc1417055
amr-devops <52605701+amr-devops@users.noreply.github.com>
2022-04-26 18:32:26 +0300

    Update __manifest__.py

--- EasyERPS_pos_auto_print_kitchen_receipt/__manifest__.py
+++ EasyERPS_pos_auto_print_kitchen_receipt/__manifest__.py
@@ -7,7 +7,7 @@
         Auto print kitchen receipt after validate
         """,
     'author': "EasyERPS",
-    'website': "https://www.EasyERPS.com",
+    'website': "https://easyerps.com",
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
