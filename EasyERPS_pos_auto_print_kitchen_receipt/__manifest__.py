e9138a995cbadb5712bc012752855686baffd575
amr-devops <amr.devops@gmail.com>
2022-04-26 14:45:12 +0300

    [ADD] auto print kitchen receipt

---
+++ EasyERPS_pos_auto_print_kitchen_receipt/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "EasyERPS POS auto print kitchen receipt",
+    'support': "support@easyerps.com",
+    'license': "LGPL-3",
+    'summary': """
+        Auto print kitchen receipt after validate
+        """,
+    'author': "EasyERPS",
+    'website': "www.EasyERPS.com",
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Point of Sale',
+    'version': '15.0.1',
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'point_of_sale', 'pos_restaurant'],
+    'assets': {
+        'point_of_sale.assets': [
+            'EasyERPS_pos_auto_print_kitchen_receipt/static/src/js/ReceiptScreen.js',
+            'EasyERPS_pos_auto_print_kitchen_receipt/static/src/js/Models.js',
+        ],
+    },
+
+}
